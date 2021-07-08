07caf2a47616acb795cd60e8cc4ddffc512feca7
Akins <82357502+Akins2229@users.noreply.github.com>
2021-07-07 22:45:53 -0500

    Create main.py
    dict part 1

---
+++ src/main.py
@@ -0,0 +1,21 @@
+"""
+M. Akins 2021
+
+
+Economic Policy Change Index
+
+
+M.I.T License
+"""
+
+#imports
+import os
+
+#constant variables dictionary
+#note: all of the values will be stored under number (in string) identifiers to make calling certain values easier later on. If you need a guide to find what value means what, reference our README (note: not finished yet becasuse I haven't finished coding this yet)
+
+variables = {
+  '1': 1814.27, #food consumed per person per day in grams (4 pounds)
+  '2': 3.7, #water consumed per person per day in litres (15.5 cups)
+  '3': 336.987, #water used (non-drunken) by person per day in litres (89 gallons)
+}
