d14a45ab6cc2e3a2b1caa6c6737a4bea34cbf869
Akins <82357502+Akins2229@users.noreply.github.com>
2021-10-25 09:04:04 -0700

    Update main.py
    build classes

--- src/main.py
+++ src/main.py
@@ -8,14 +8,51 @@ Economic Policy Change Index
 M.I.T License
 """
 
-#imports
+import typing
+import statistics
 import os
+import json
 
-#constant variables dictionary
-#note: all of the values will be stored under number (in string) identifiers to make calling certain values easier later on. If you need a guide to find what value means what, reference our README (note: not finished yet becasuse I haven't finished coding this yet)
+class Country:
+  def __init__(
+    self,
+    budget: int,
+    population: int,
+    gdp: float,
+    name: str,
+    description: str,
+    flag: str # filepath to assets flag (may use .assets.constants.Flags class)
+  ) -> None:
+    self.STARTING_BUDGET = budget
+    self.money_spent = 0 # money spent by a country in the last year
+    self.budget = budget - self.money_spent
+    self.population = population
+    self.gdp = gdp
+    self.name = name
+    self.description = description
+    self.flag=flag
 
-variables = {
-  '1': 1814.27, #food consumed per person per day in grams (4 pounds)
-  '2': 3.7, #water consumed per person per day in litres (15.5 cups)
-  '3': 336.987, #water used (non-drunken) by person per day in litres (89 gallons)
-}
+    
+
+class Game:
+  def __init__(
+    self,
+    countries: typing.List[Country]
+  ) -> None:
+    gdps = []
+    budgets = []
+    for country in countries:
+      gdps.append(country.gdp)
+      budgets.append(country.budget)
+    self.AVERAGE_GDP = statistics.fmean(gdps)
+    self.GLOBAL_BUDGET = statistics.mean(budgets)
+    self.money_spent = 0 # rate of how much money has been spent this year globally
+    self.available_budget = self.GLOBAL_BUDGET - self.money_spent
+
+class GameState(Game):
+  def __init__(
+    self,
+    **kwargs
+  ):
+    super().__init__(**kwargs)
+    
